fc453ef7287ac8d43de6f660e3c42378634462a7
stromboli1 <54105754+stromboli1@users.noreply.github.com>
2023-04-03 16:00:13 +0200

    WIP. Added main.py

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
 import _thread
-import utime
+import time
+import network
 import decompile_packet as dp
 import sockettransmission_receiver as strev
 
-def socket_thread():
+SSID = 'supercoolnetwork'
+PASSWD = 'letmeinfuck'
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+def ap_connect() -> None:
+    """Connect to access point
+
+    Args:
+
+    Returns:
+        None:
+    """
+    wlan.connect(SSID, PASSWD)
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+
+        time.sleep_ms(1000)
+
+    if wlan.status() != 3:
+        raise RuntimeError('Network Connection Failed')
+
+ap_connect()
+
+
+def socket_thread() -> None:
+    """Socket thread for recieving packets
+
+    Args:
+
+    Returns:
+        None:
+    """
     while True:
         packet = strev.receive_packet()
         clk, paramlist, devices = dp.decompile(packet)
         print(clk, paramlist, devices)
 
-thread = _thread.start_new_thread(socket_thread(), ())
+thread = _thread.start_new_thread(socket_thread, ())
 
 while True:
-    utime.sleep(5)
+    time.sleep(5)
     print('Still running')
--- sockettransmission_receiver.py
+++ sockettransmission_receiver.py
@@ -3,9 +3,12 @@ import socket
 PORT = 42069
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.setblocking(0)
 soc.bind(('0.0.0.0',PORT))
 soc.listen(1)
 
 def receive_packet():
-    c, a = soc.accept()
-    return c.recv(2048)
+    c, _ = soc.accept()
+    data = c.recv(2048)
+    c.close()
+    return data
