1186b7c85c04d828120b418396f0a72350355d60
Musvon2705 <Musvon2705@gmail.com>
2023-04-25 14:01:19 +0200

    Major fix to model test
    Major fix to the test. The test now works more or less.

--- model_test.py
+++ model_test.py
@@ -1,7 +1,8 @@
 import models as mod
 import matplotlib.pyplot as plt
 
-oven_coeff = [ 2.91224354e-02,  3.43245148e-02, -7.41751676e-02, 4.77055793e-02, -1.49646564e-02, 2.71775105e-03, -3.03456792e-04,  2.09968211e-05, -8.72185305e-07,  1.98402817e-08, -1.89451849e-10]
+oven_coeff = [3.23654726e-02, -8.87464977e-02, 1.10875118e-01, -5.55039379e-02, 1.41481147e-02, -2.00354900e-03, 1.63395561e-04, -7.60766518e-06,
+ 1.87551879e-07, -1.89766897e-09]
 
 dryer_coeff = [ 1.79708907e-02,  3.55758062e-02, -5.97575512e-02,  3.11564699e-02,
 -7.99325968e-03,  1.19446290e-03, -1.10903593e-04,  6.46081210e-06,
@@ -22,22 +23,40 @@ heatpump = mod.Heatpump(1.5, 0.2, True, heating_multiplier=3, heating_fluctuatio
 house = mod.House('d', 150, 2.8, 21, 0, 212, [oven, dryer, heatpump], bg_coeff, 0.05)
 
 comp_list = []
+on_list = []
 average = []
 
 for i in range(1440):
     average.append(i)
 
-for i in range(200):
-    house.update_time(60)
-    print(house.tick())
 
+for n in range(100):
+    day_list = []
+    for i in range(1440):
+        house.update_time(60)
+        state, draw, temp = house.tick()
+        day_list.append(state)
+    on_list.append(day_list)
+
+for n in range(100):
+    print(on_list[n][1379])
 
 """
+for n in range(100):
+    day_list = []
+    for i in range(1440):
+        house.update_time(60)
+        state, draw, temp = house.tick()
+        day_list.append(draw)
+    comp_list.append(day_list)
+
+
+
 for i in range(1440):
     sum = 0
-    for n in range(1000):
+    for n in range(100):
         sum += comp_list[n][i]
-    average[i] = sum/1000
+    average[i] = sum/100
 
 x_list = []
 
