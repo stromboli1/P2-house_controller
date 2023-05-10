8257e6605d5e451af23ae9cd2ccf6ca03bc06b11
TheEngbjerg <nicolaiengbjerg@gmail.com>
2023-05-10 15:34:07 +0200

    added comments

--- start_receiver.py
+++ start_receiver.py
@@ -1,15 +1,35 @@
+# imports
 import socket
 
+# create socket
 start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 start_sock.bind(('', 6969))
 
 def receive_start() -> bool:
+    """function for starting data transfer.
+
+    Args:
+
+    Returns:
+        bool:
+    """
+
+    # returns true if start signal is received
     if start_sock.recv(1024):
         start_sock.close()
         return True
     return False
 
 def receive_stop() -> bool:
+    """function for receiving stop signal.
+
+    Args:
+
+    Returns:
+        bool:
+    """
+
+    # returns true if stop signal is received
     if not start_sock.recv(1024):
         start_sock.close()
         return True
