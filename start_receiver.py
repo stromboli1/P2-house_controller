90abd41c95ae47c60a80066b62db932a66281f7f
TheEngbjerg <nicolaiengbjerg@gmail.com>
2023-05-10 14:00:24 +0200

    fixed minor bug

--- start_receiver.py
+++ start_receiver.py
@@ -1,8 +1,9 @@
 import socket
 
+start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+start_sock.bind(('', 6969))
+
 def receive_start() -> bool:
-    start_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    start_sock.bind(('', 6969))
     if start_sock.recv(1024):
         start_sock.close()
         return True
