aad8e259561678ed393e0f42e90d433cc1316d4a
Musvon2705 <Musvon2705@gmail.com>
2023-05-11 10:31:10 +0200

    Made json files for coeffs and appliance data

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ from time import sleep
 from communication_utils import decompile_packet, datatrans_packetinator, receive_signal
 from models import House, Heatpump, Oven, Dryer
 
+
 # GLOBAL VARS
 CONTROLPROTOCOLPORT: int = 42069
 STOPTHREADS = False
+
+with open('coefficients.json', 'r') as fd:
+    coefficients = json.load(fd)
+
 
 # Global sockets (CANNOT be recovered if they crash)
 datasock: socket.socket = socket.socket(
@@ -57,9 +62,9 @@ def receive_controlpacket() -> Optional[tuple[int, int, dict, int]]:
         return None
 
 # Make Appliances and House (TODO: Make the contants defined somewhere else, controlprotocol maybe?)
-oven_coeff = [5.11972665e-04, -7.03402445e-04,  7.68026707e-04, -3.66363583e-04, 8.96781866e-05, -1.14300653e-05, 7.10339539e-07, -1.23448103e-08, -8.17581893e-10, 4.38334209e-11, -6.15768582e-13]
-dryer_coeff = [2.99514846e-04, 5.92930103e-04, -9.95959187e-04, 5.19274499e-04, -1.33220995e-04, 1.99077151e-05, -1.84839322e-06, 1.07680202e-07, -3.80412797e-09, 7.40399062e-11, -6.06187573e-13]
-bg_coeff = [ 2.99994599e-01, -5.51791329e-04, -4.13148994e-02,  2.10030766e-02, -4.10493904e-03,  4.10768972e-04, -2.29920196e-05,  7.26545140e-07, -1.20913754e-08,  8.21833884e-11]
+oven_coeff = coefficients["oven"]
+dryer_coeff = coefficients["dryer"]
+bg_coeff = coefficients["background"]
 
 # Creating oven appliance for house
 oven = Oven(power_usage=1.1, power_fluctuation=0.02, controllable=False, state_coeffs=oven_coeff, allowed_cycles=1, cycle_time_range=(30, 120))
